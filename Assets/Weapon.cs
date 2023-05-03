ce6808ca6e41bfc53886953d4ab54f16edc7a19e
Atta Jirofty <jirofty76@gmail.com>
2023-05-03 17:09:36 -0400

    Added Gun SFX

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -7,13 +7,31 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] private Transform firePoint;
     [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private AudioClip[] gunSfx;
+    [SerializeField] private Animator animator;
+
+    [SerializeField] private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
+            PlayRandomGunSound();
+            animator.SetBool("isShooting", true);
         }
-        
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            animator.SetBool("isShooting", false);
+
+        }
+
     }
 
     private void Shoot()
@@ -21,5 +39,11 @@ public class Weapon : MonoBehaviour
         // Shooting Logic
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         //throw new NotImplementedException();
+    }
+
+    void PlayRandomGunSound()
+    {
+        audioSource.clip = gunSfx[UnityEngine.Random.Range(0, gunSfx.Length)];
+        audioSource.Play();
     }
 }
